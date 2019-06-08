25675d1b5fd3a3f45c70ee35cf9c62a4bc33cdcd
Jordan Roberts <jordanroberts@talk21.com>
2019-06-08 14:02:46 +0100

    Added maximum quality to increaseQuality function to refactor

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -3,6 +3,7 @@ class Item {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
+    this.maxQuality = 50;
   }
 }
 
@@ -15,52 +16,42 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         this.decreaseQuality();
       } else {
-        if (this.items[i].quality < 50) {
           this.increaseQuality();
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.increaseQuality();
-              }
+              this.increaseQuality();
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.increaseQuality();
-              }
+              this.increaseQuality();
             }
           }
         }
-      }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.decreaseQuality();
-              }
-            }
+            this.decreaseQuality();
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.increaseQuality();
-          }
+          this.increaseQuality();
         }
       }
     }
-
     return this.items;
   }
 
   increaseQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      this.items[i].quality += 1
+      if (this.items[i].quality < 50) {
+        this.items[i].quality += 1
+      }
     }
   }
+
 
   decreaseQuality() {
     for (var i = 0; i < this.items.length; i++ ) {
