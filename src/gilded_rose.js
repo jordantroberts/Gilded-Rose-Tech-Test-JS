d31b7480d4706a91014c2acf21ee8d77ba47f7f8
Jordan Roberts <jordanroberts@talk21.com>
2019-06-08 15:25:40 +0100

    Deleted repeated loops

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,18 +4,18 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    this.updateSellIn()
     for (var i = 0; i < this.items.length; i++) {
-      this.decreaseQuality()
+      this.updateSellIn(i)
+      this.decreaseQuality(i)
       if(this.items[i].sellIn < 0) {
-        this.decreaseQuality()
+        this.decreaseQuality(i)
       }
       if (this.items[i].name == 'Aged Brie') {
         this.updateBrie(i);
       }
       if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.increaseQuality();
-        this.increaseQuality();
+        this.increaseQuality(i);
+        this.increaseQuality(i);
         this.updateBackstage(i)
         }
       }
@@ -23,46 +23,40 @@ class Shop {
   }
 
   updateBrie(index) {
-    this.increaseQuality();
+    this.increaseQuality(index);
     if(this.items[index].sellIn < 0) {
-      this.increaseQuality()
+      this.increaseQuality(index)
     }
   }
 
   updateBackstage(index) {
     if(this.items[index].sellIn < 11) {
-      this.increaseQuality();
+      this.increaseQuality(index);
     }
     if(this.items[index].sellIn < 6) {
-      this.increaseQuality();
+      this.increaseQuality(index);
     }
     if(this.items[index].sellIn < 0) {
       this.items[index].quality = 0
     }
   }
 
-  updateSellIn() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+  updateSellIn(index) {
+    if (this.items[index].name != 'Sulfuras, Hand of Ragnaros') {
+      this.items[index].sellIn = this.items[index].sellIn - 1;
     }
   }
 
-  increaseQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].quality < 50) {
-        this.items[i].quality += 1
-      }
+  increaseQuality(index) {
+    if (this.items[index].quality < 50) {
+      this.items[index].quality += 1
     }
   }
 
-  decreaseQuality() {
-    for (var i = 0; i < this.items.length; i++ ) {
-      if ((this.items[i].name != 'Aged Brie') && (this.items[i].name != 'Sulfuras, Hand of Ragnaros')) {
-        if (this.items[i].quality > 0) {
-          this.items[i].quality -= 1
-        }
+  decreaseQuality(index) {
+    if ((this.items[index].name != 'Aged Brie') && (this.items[index].name != 'Sulfuras, Hand of Ragnaros')) {
+      if (this.items[index].quality > 0) {
+        this.items[index].quality -= 1
       }
     }
   }
